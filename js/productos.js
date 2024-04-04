38df09c36d0f5caa14b7a7683ec88d016b677779
Juan Ramos <juanferasa@gmail.com>
2024-04-03 23:38:32 -0500

    tercera pre_entrega

---
+++ js/productos.js
@@ -0,0 +1,42 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Acer Nitro 5",
+        precio: 950,
+        img:
+            "./images/acer-nitro-5.webp",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "Apple Macbook Air",
+        precio: 980,
+        img:
+            "./images/apple-macbook-air.webp",
+        cantidad: 1,
+    },
+    {
+        id: 3,
+        nombre: "Lenovo Legion",
+        precio: 890,
+        img:
+            "./images/lenovo-legion.webp",
+        cantidad: 1,
+    },
+    {
+        id: 4,
+        nombre: "HP",
+        precio: 570,
+        img:
+            "./images/hp.webp",
+        cantidad: 1,
+    },
+    {
+        id: 5,
+        nombre: "Dell Inspiron",
+        precio: 460,
+        img:
+            "./images/dell-inspiron.webp",
+        cantidad: 1,
+    },
+];
